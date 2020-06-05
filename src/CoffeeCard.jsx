8022c6062498e6671ad25932fd52108246ead3bf
hoehoetester <hoehoetester1@gmail.com>
2020-06-06 00:11:59 +1200

    card layout

--- src/CoffeeCard.jsx
+++ src/CoffeeCard.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Avatar from "@material-ui/core/Avatar";
+import ShareIcon from "@material-ui/icons/Share";
+
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { CardMedia } from "@material-ui/core";
 
 const useStyles = makeStyles({
     bullet: {
@@ -19,33 +27,44 @@ const useStyles = makeStyles({
         marginBottom: 12,
     },
 });
-const CoffeeCard = () => {
+const CoffeeCard = (props) => {
     const classes = useStyles();
     const bull = <span className={classes.bullet}>•</span>;
+    const { avatarUrl, title, subtitle, description, imageUrl } = props;
     return (
         <Card>
+            <CardHeader
+                avatar={
+                    <Avatar
+                        aria-label="recipe"
+                        className={classes.avatar}
+                        src={avatarUrl}
+                    >
+                        R
+                    </Avatar>
+                }
+                action={
+                    <IconButton aria-label="settings">
+                        <ShareIcon />
+                    </IconButton>
+                }
+                title={title}
+                subheader={subtitle}
+            />
+            <CardMedia
+                className={classes.media}
+                style={{ height: "150px" }}
+                image={imageUrl}
+                title={imageUrl}
+            />
             <CardContent>
-                <Typography
-                    className={classes.title}
-                    color="textSecondary"
-                    gutterBottom
-                >
-                    Word of the Day
-                </Typography>
-                <Typography variant="h5" component="h2">
-                    be{bull}nev{bull}o{bull}lent
-                </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                    adjective
-                </Typography>
                 <Typography variant="body2" component="p">
-                    well meaning and kindly.
-                    <br />
-                    {'"a benevolent smile"'}
+                    {description}
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small">Learn More</Button>
+                <Button size="small">Buy Now</Button>
+                <Button size="small">Offer</Button>
             </CardActions>
         </Card>
     );
--- src/Content.jsx
+++ src/Content.jsx
@@ -7,22 +7,13 @@ const Content = () => {
         <div>
             <Grid container spacing={4}>
                 <Grid item xs={12} sm={4}>
-                    <CoffeeCard />
-                </Grid>
-                <Grid item item xs={12} sm={4}>
-                    <CoffeeCard />
-                </Grid>
-                <Grid item item xs={12} sm={4}>
-                    <CoffeeCard />
-                </Grid>
-                <Grid item item xs={12} sm={4}>
-                    <CoffeeCard />
-                </Grid>
-                <Grid item item xs={12} sm={4}>
-                    <CoffeeCard />
-                </Grid>
-                <Grid item item xs={12} sm={4}>
-                    <CoffeeCard />
+                    <CoffeeCard
+                        title="Keurig K-Duo"
+                        subtitle="234,324"
+                        description="Use both ground coffee and k-cup pods. Multiple brew sizes: brew an 8, 10, or 12-cup carafe and an 237, 296, or 355ml (8, 10, or 12 oz. ) cup "
+                        avatarUrl="https://upload.wikimedia.org/wikipedia/commons/e/e0/Keurig_Logo.png"
+                        imageUrl="https://images-na.ssl-images-amazon.com/images/I/61-KUPluVYL._AC_SL1500_.jpg"
+                    />
                 </Grid>
             </Grid>
         </div>
