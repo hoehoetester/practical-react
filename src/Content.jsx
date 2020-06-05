a46ce0bcd1f621c6c46d575c2358912744cfa68a
hoehoetester <hoehoetester1@gmail.com>
2020-06-06 00:20:23 +1200

    card data to display

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import CoffeeCard from "./CoffeeCard";
 import { Grid } from "@material-ui/core";
+import coffeeMakerList from "./constant";
 
 const Content = () => {
+    const getCoffeeMakerCard = (coffeeCardObj) => {
+        return (
+            <Grid item xs={12} sm={4}>
+                <CoffeeCard {...coffeeCardObj} />
+            </Grid>
+        );
+    };
     return (
         <div>
             <Grid container spacing={4}>
-                <Grid item xs={12} sm={4}>
-                    <CoffeeCard
-                        title="Keurig K-Duo"
-                        subtitle="234,324"
-                        description="Use both ground coffee and k-cup pods. Multiple brew sizes: brew an 8, 10, or 12-cup carafe and an 237, 296, or 355ml (8, 10, or 12 oz. ) cup "
-                        avatarUrl="https://upload.wikimedia.org/wikipedia/commons/e/e0/Keurig_Logo.png"
-                        imageUrl="https://images-na.ssl-images-amazon.com/images/I/61-KUPluVYL._AC_SL1500_.jpg"
-                    />
-                </Grid>
+                {coffeeMakerList.map((coffeeMakeObj) =>
+                    getCoffeeMakerCard(coffeeMakeObj)
+                )}
             </Grid>
         </div>
     );
