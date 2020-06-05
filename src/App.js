c6a5d0aff5d3355a09e5352fdb05e26b0f4951c7
hoehoetester <hoehoetester1@gmail.com>
2020-06-05 22:57:28 +1200

    page layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./App.css";
+import { Grid } from "@material-ui/core";
 
 function App() {
     return (
-        <div className="App">
-            <h1>React</h1>
+        <div>
+            <Grid container direction="column">
+                <Grid item>Lorem, ipsum dolor.</Grid>
+                <Grid item container>
+                    <Grid item xs={0} sm={2} />
+                    <Grid item xs={12} sm={8}>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. In soluta repellendus quas, quisquam tempora quam.
+                        Pariatur itaque natus nostrum excepturi similique.
+                        Minima adipisci impedit cum dolores eius reprehenderit
+                        corporis? Reiciendis.
+                    </Grid>
+                    <Grid item xs={0} sm={2} />
+                </Grid>
+            </Grid>
         </div>
     );
 }
